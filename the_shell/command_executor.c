f07c4625d105d2e32fe132689bf5544bdc5815b3
anyango-okoth <anyangomoureen02@gmail.com.com>
2023-06-28 01:42:37 -0700

    execommand

--- the_shell/command_executor.c
+++ the_shell/command_executor.c
@@ -6,33 +6,63 @@
 #include <sys/wait.h>
 #include "main.h"
 
-void executeCommand(char *args[], char *envp[]) {
-    if (args[0] != NULL) {
-        if (strcmp(args[0], "cd") == 0) {
-            if (args[1] == NULL || strcmp(args[1], "~") == 0) {
+/**
+ * executeCommand - Executes commands provided in the @args array within the context
+ *                 of a shell environment.
+ *
+ * @args: An array of command arguments.
+ * @envp: The environment variables (unused).
+ *
+ * Description:
+ * This function supports two types of command "cd" and general commands.
+ * cd command changes the directory based on args[1]
+ * general command creates a child process using using fork()
+ */
+
+void executeCommand(char *args[], char *envp[]) 
+{
+    if (args[0] != NULL) 
+    {
+        if (strcmp(args[0], "cd") == 0) 
+	{
+            /*Handle 'cd' command separately*/
+            if (args[1] == NULL || strcmp(args[1], "~") == 0) 
+	    {
                 args[1] = getenv("HOME");
-            } else if (strcmp(args[1], "-") == 0) {
+            } 
+	    else if (strcmp(args[1], "-") == 0) 
+	    {
                 args[1] = getenv("OLDPWD");
             }
 
-            if (chdir(args[1]) == 0) {
+            if (chdir(args[1]) == 0) 
+	    {
+                /*Update current directory environment variables*/
                 char currentDir[1024];
                 getcwd(currentDir, sizeof(currentDir));
                 setenv("PWD", currentDir, 1);
                 setenv("OLDPWD", getenv("PWD"), 1);
-            } else {
+            } 
+	    else 
+	    {
                 fprintf(stderr, "Failed to change directory\n");
             }
-        } else {
+        } 
+	else {
+            /*Fork and execute the command*/
             pid_t pid = fork();
-            if (pid < 0) {
+            if (pid < 0) 
+	    {
                 fprintf(stderr, "Fork failed\n");
                 exit(EXIT_FAILURE);
             } else if (pid == 0) {
+                /*Child process executes the command*/
                 execvp(args[0], args);
                 fprintf(stderr, "Failed to execute command\n");
                 exit(EXIT_FAILURE);
-            } else {
+            } 
+	    else {
+                /*Parent process waits for the child to complete*/
                 wait(NULL);
             }
         }
